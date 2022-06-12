71d931c5c8c7eadfc3e6dc41da9bf7110002a2d0
GiovaTC <90740161+GiovaTC@users.noreply.github.com>
2022-06-12 09:28:54 -0500

    Add files via upload

---
+++ HellowWorld/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String saludar="Hola como estas";
+        System.out.println(saludar + " Primer proyecto ");
+        System.out.println(saludar.toUpperCase());
+        int numero = 10;
+        System.out.println("Es un numero:"+numero);
+        float numeroDecmimal=12.02f;
+        System.out.printf("Es un numero flotante:"+ numeroDecmimal);
+        double numeroDouble=12.25;
+        System.out.println("Es un numero double:"+ numeroDouble);
+        boolean valor = true;
+    }
+}
